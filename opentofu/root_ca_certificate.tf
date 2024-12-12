9d36bf7367754a94494099f6cdfecac5e59e73ef
Saad Ali <engr.saadali786@gmail.com>
2024-12-12 16:40:39 +0500

    Add an intermediate CA certificate and create a role

--- opentofu/root_ca_certificate.tf
+++ opentofu/root_ca_certificate.tf
@@ -1,10 +1,18 @@
+# Enable the PKI secrets engine
+resource "vault_mount" "nixknight_ca_authority" {
+  path                      = "nixknight_ca_authority"
+  type                      = "pki"
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 315360000 # 10 years
+}
+
 # Generate root CA
 resource "vault_pki_secret_backend_root_cert" "nixknight_ca" {
   depends_on = [ vault_mount.nixknight_ca_authority ]
 
   issuer_name = "NIXKNIGHT-Root-Issuer"
 
-  common_name  = "NIXKNIGHT Root CA"
+  common_name  = "nixknight.net"
   organization = "NIXKNIGHT"
   country      = "PK"
   locality     = "Lahore"
@@ -17,9 +25,38 @@ resource "vault_pki_secret_backend_root_cert" "nixknight_ca" {
   key_bits = 4096
 }
 
-# Configure the CA's URLs
-resource "vault_pki_secret_backend_config_urls" "nixknight_ca" {
-  backend                 = vault_mount.nixknight_ca_authority.path
-  issuing_certificates    = [ "http://${var.vault_address}/v1/${vault_mount.nixknight_ca_authority.path}/ca" ]
-  crl_distribution_points = [ "http://${var.vault_address}/v1/${vault_mount.nixknight_ca_authority.path}/crl" ]
+# Generate intermediate CA using the root CA
+resource "vault_pki_secret_backend_issuer" "nixknight_ca_intermediate" {
+   backend                        = vault_mount.nixknight_ca_authority.path
+   issuer_ref                     = vault_pki_secret_backend_root_cert.nixknight_ca.issuer_id
+   issuer_name                    = vault_pki_secret_backend_root_cert.nixknight_ca.issuer_name
+   revocation_signature_algorithm = "SHA256WithRSA"
+}
+
+# Set role with proper constraints
+resource "vault_pki_secret_backend_role" "nixknight_ca_intermediate" {
+  name    = "NIXKNIGHT-Intermediate-Root-CA-Role"
+
+  backend         = vault_mount.nixknight_ca_authority.path
+  issuer_ref       = vault_pki_secret_backend_issuer.nixknight_ca_intermediate.issuer_ref
+
+  allow_any_name = true
+  enforce_hostnames = false
+  allow_localhost = false
+  allow_wildcard_certificates = false
+  key_type = "rsa"
+  key_bits = 4096
+
+  key_usage = [
+    "digitalSignature",
+    "nonRepudiation",
+    "keyEncipherment",
+    "dataEncipherment"
+  ]
+
+  allowed_domains  = ["vms.nixknight.net"]
+  allow_subdomains = true
+
+  max_ttl = "315360000"
+  require_cn = false
 }
--- opentofu/vault_mount.tf
+++
@@ -1,7 +0,0 @@
-# Enable the PKI secrets engine
-resource "vault_mount" "nixknight_ca_authority" {
-  path                      = "nixknight_ca_authority"
-  type                      = "pki"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 315360000 # 10 years
-}
